72949a3519687efa38105cef146316b770cf97a5
steedn <steedn@outlook.com>
2020-10-17 19:37:24 -0600

    changes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,10 +8,21 @@ class GamesController < ApplicationController
       @game = Game.new
     end
 
+    def create
+        @game = Game.new(game_params)
+        if @game.valid?
+          @game.save  
+
+          redirect_to game_path(@game)
+        else 
+            render :new
+        end
+    end
+
 
     private 
 
-    def games_params
-        params
+    def game_params
+        params.require(:game).permit(:title, :genre)
     end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,12 @@
 class Game < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
+
+    validates :title, presence: true
+    validates :genre, presence: true
+    validates :price, presence: true
+    validates :platform, presence: true
+    validates :price, numericality: true
+
+    
 end
